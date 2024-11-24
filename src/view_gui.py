c80c1c1752eee02fff75742bfc1c840371c8742f
adrianapps <bulsar@tutanota.com>
2024-11-24 01:42:47 +0100

    add FilterExpense and plot

--- src/view_gui.py
+++ src/view_gui.py
@@ -1,7 +1,8 @@
 from datetime import datetime
-from math import exp
 import customtkinter
 from CTkTable import *
+import matplotlib.pyplot as plt
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 
 customtkinter.set_appearance_mode("dark")
 customtkinter.set_default_color_theme("green")
@@ -12,7 +13,7 @@ class App(customtkinter.CTk):
         super().__init__()
         self.controller = controller
 
-        self.geometry("900x600")
+        self.geometry("900x700")
 
         sidebar_frame = customtkinter.CTkFrame(self, width=150)
         sidebar_frame.pack(side="left", fill="y", padx=10, pady=10)
@@ -25,21 +26,50 @@ class App(customtkinter.CTk):
         filter_button = customtkinter.CTkButton(sidebar_frame, text="Filter", command=self.filter_expenses)
         filter_button.pack(pady=10)
 
-        self.table_frame = customtkinter.CTkFrame(self)
+        self.table_frame = customtkinter.CTkScrollableFrame(self)
         self.table_frame.pack(expand=True, fill="both", padx=20, pady=10)
 
         self.create_table()
 
-    def create_table(self):
-        self.expenses = self.controller.get_expenses()
-        self.expenses_list = [
-            [expense.id, expense.title, expense.price, expense.date] for expense in self.expenses
+        self.plot_frame = customtkinter.CTkFrame(self)
+        self.plot_frame.pack(expand=True, fill="both", padx=20, pady=10)
+        self.create_plot()
+
+    def create_plot(self, expenses=None):
+        if expenses is None:
+            expenses = self.controller.get_expenses()
+
+        prices = [expense.price for expense in expenses]
+        dates = [expense.date for expense in expenses]
+
+        for widget in self.plot_frame.winfo_children():
+            widget.destroy()
+
+        plt.style.use('dark_background')
+        fig, ax = plt.subplots(figsize=(8, 6))
+
+        ax.scatter(dates, prices, color="green")
+
+        ax.set_xlabel('Date')
+        ax.set_ylabel('Price')
+        ax.set_title('Expense Prices Over Time')
+
+        canvas = FigureCanvasTkAgg(fig, master=self.plot_frame)
+        canvas.draw()
+        canvas.get_tk_widget().pack()
+
+    def create_table(self, expenses=None):
+        if expenses is None:
+            expenses = self.controller.get_expenses()
+
+        expenses_list = [
+            [expense.id, expense.title, expense.price, expense.date] for expense in expenses
         ]
 
         for widget in self.table_frame.winfo_children():
             widget.destroy()
 
-        for index, expense_data in enumerate(self.expenses_list):
+        for index, expense_data in enumerate(expenses_list):
             row_frame = customtkinter.CTkFrame(self.table_frame)
             row_frame.pack(fill="x", pady=5)
 
@@ -60,17 +90,18 @@ class App(customtkinter.CTk):
 
     def add_expense(self):
         AddExpense(self, self.controller)
-    
+
     def update_expense(self, expense_id):
         expense = self.controller.get_expense_detail(expense_id)
         UpdateExpense(self, self.controller, expense)
-    
+
     def delete_expense(self, expense_id):
         self.controller.delete_expense(expense_id)
         self.create_table()
-    
+        self.create_plot()
+
     def filter_expenses(self):
-        pass
+        FilterExpenses(self, self.controller)
 
 
 class AddExpense(customtkinter.CTkToplevel):
@@ -92,7 +123,7 @@ class AddExpense(customtkinter.CTkToplevel):
         self.price_entry.pack(pady=5)
 
         today_date = datetime.now().strftime("%Y-%m-%d")
-        self.date_label = customtkinter.CTkLabel(self, text="Price:")
+        self.date_label = customtkinter.CTkLabel(self, text="Date:")
         self.date_label.pack(pady=5)
         self.date_entry = customtkinter.CTkEntry(self, placeholder_text=today_date)
         self.date_entry.pack(pady=5)
@@ -109,6 +140,7 @@ class AddExpense(customtkinter.CTkToplevel):
 
         self.controller.add_expense(title, price, date)
         self.master.create_table()
+        self.master.create_plot()
 
         self.destroy()
 
@@ -158,5 +190,76 @@ class UpdateExpense(customtkinter.CTkToplevel):
 
         self.controller.update_expense(self.expense.id, expense_data)
         self.master.create_table()
+        self.master.create_plot()
+
+        self.destroy()
+
+
+class FilterExpenses(customtkinter.CTkToplevel):
+    def __init__(self, parent, controller):
+        super().__init__(parent)
+        self.controller = controller
+
+        self.geometry("400x300")
+        self.title("Filter Expenses")
+
+        self.title_label = customtkinter.CTkLabel(self, text="Title:")
+        self.title_label.pack(pady=5)
+        self.title_entry = customtkinter.CTkEntry(self)
+        self.title_entry.pack(pady=5)
+
+        price_frame = customtkinter.CTkFrame(self)
+        price_frame.pack(pady=5)
+
+        self.min_price_label = customtkinter.CTkLabel(price_frame, text="Min Price:")
+        self.min_price_label.grid(row=0, column=0, padx=5, pady=5)
+        self.min_price_entry = customtkinter.CTkEntry(price_frame)
+        self.min_price_entry.grid(row=1, column=0, padx=5, pady=5)
+
+        self.max_price_label = customtkinter.CTkLabel(price_frame, text="Max Price:")
+        self.max_price_label.grid(row=0, column=1, padx=5, pady=5)
+        self.max_price_entry = customtkinter.CTkEntry(price_frame)
+        self.max_price_entry.grid(row=1, column=1, padx=5, pady=5)
+
+        date_frame = customtkinter.CTkFrame(self)
+        date_frame.pack(pady=5)
+
+        self.min_date_label = customtkinter.CTkLabel(date_frame, text="Min Date:")
+        self.min_date_label.grid(row=0, column=0, padx=5, pady=5)
+        self.min_date_entry = customtkinter.CTkEntry(date_frame)
+        self.min_date_entry.grid(row=1, column=0, padx=5, pady=5)
+
+        self.max_date_label = customtkinter.CTkLabel(date_frame, text="Max Date:")
+        self.max_date_label.grid(row=0, column=1, padx=5, pady=5)
+        self.max_date_entry = customtkinter.CTkEntry(date_frame)
+        self.max_date_entry.grid(row=1, column=1, padx=5, pady=5)
+
+        self.save_button = customtkinter.CTkButton(
+            self, text="Apply", command=self.apply_filter
+        )
+        self.save_button.pack(pady=10)
+
+    def apply_filter(self):
+        title = self.title_entry.get()
+        min_price = self.min_price_entry.get()
+        max_price = self.max_price_entry.get()
+        min_date = self.min_date_entry.get()
+        max_date = self.max_date_entry.get()
+
+        min_price = float(min_price) if min_price else None
+        max_price = float(max_price) if max_price else None
+        min_date = datetime.strptime(min_date, "%Y-%m-%d") if min_date else None
+        max_date = datetime.strptime(max_date, "%Y-%m-%d") if max_date else None
+
+        filtered_query = self.controller.filter_expense(
+            title=title,
+            min_price=min_price,
+            max_price=max_price,
+            min_date=min_date,
+            max_date=max_date
+        )
+
+        self.master.create_table(filtered_query)
+        self.master.create_plot(filtered_query)
 
         self.destroy()
